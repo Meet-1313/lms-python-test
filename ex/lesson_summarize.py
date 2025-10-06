37f6181f70b47834c541e10846ce9114da36fefc
Meet-1313 <solankimeet@1779gmail.com>
2025-10-06 19:48:49 +0530

    added user input functionality

--- ex/lesson_summarize.py
+++ ex/lesson_summarize.py
@@ -7,11 +7,7 @@ def summarize_lesson(lesson_text, num_sentences=2):
     summary = '.'.join(sentences[:num_sentences])
     return summary 
 if __name__ == "__main__":
-    lesson = """python is a versatile programming language . it is widely used in web develoment,data science,ai and more
-    Learning python can opne many opporttunitites 
-    this script create simple summary of lessons"""
-
-
+    lesson = input ("enter your leeson text: ")
     summary = summarize_lesson(lesson)
     print("Lesson Summary:")
     print (summary)
